1860b298c0645db2a349062877c91a4e34a00f5a
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-02-23 11:37:59 +0100

    2025-02-23 11:37:59

--- walidacja_glukozy.py
+++ walidacja_glukozy.py
@@ -1,1 +1,7 @@
 # Walidacja stężenia glukozy we krwi 
+# wartość wejściowa - podać w terminalu
+# mg/dL - 70 - 100 
+# mmol/L - 3.9 - 5.6
+# 1. sprawdzić, z którego zakresu podajemy wartość - wartość 40 graniczna
+# 2. zweryfikować poziom i go wypisać razem z jednostką i wartością
+ 
