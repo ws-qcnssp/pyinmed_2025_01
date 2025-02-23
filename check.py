59061d794359a8fa3c883f8f1f82e289b5c66e7a
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-02-23 10:42:24 +0100

    2025-02-23 10:42:24

--- check.py
+++ check.py
@@ -9,9 +9,12 @@ import flask
 import os
 import sys
 
-if sys.version_info[:3] == (3,11,9) and os.path.join(os.getcwd(), '.venv') in sys.executable:
+if sys.version_info[:3] == (3,11,9) and os.path.join(os.getcwd(), '.venv').lower() in sys.executable.lower():
     print('Wszystko ustawione!')
 else:
     print(f'Używasz wersji pythona {sys.version_info[:3]}, z lokalizacji {sys.executable}. Sprawdź to :)')
+
+lis = []
+
 
 
