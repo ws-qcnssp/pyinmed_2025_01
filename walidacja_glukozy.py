5c4d98ce65b4f02db0a8cb7c90c18afacc47114a
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-02-23 11:48:44 +0100

    2025-02-23 11:48:44

--- walidacja_glukozy.py
+++ walidacja_glukozy.py
@@ -4,3 +4,5 @@
 # mmol/L - 3.9 - 5.6
 # 1. sprawdzić, z którego zakresu podajemy wartość - wartość 40 graniczna
 # 2. zweryfikować poziom i go wypisać razem z jednostką i wartością
+
+wartosc = input('Wprowadź stężenie glukozy we krwi: ')
