6d0f30cdd25140e82aed0a275b6a9f5569bc556e
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-03-22 11:36:22 +0100

    2025-03-22 11:36:22

--- who_life_exp.py
+++ who_life_exp.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from numpy.polynomial.polynomial import Polynomial
+from numpy.polynomial.polynomial import polyfit
 
 df = pd.read_csv('life_expectancy.csv')
 dane = df[['YEAR','REGION','SEX','Numeric']]
@@ -11,6 +13,11 @@ print(dane)
 
 dane_m = dane.query('SEX == "MLE"')
 dane_k = dane.query('SEX == "FMLE"')
+
+b_m, a_m = polyfit(dane_m['YEAR'], dane_m['Numeric'], 1)
+b_k, a_k = polyfit(dane_k['YEAR'], dane_k['Numeric'], 1)
+
+print(f'Dopasowana funkcja dla mężczyzn: f(x) = {a_m} * x + {b_m}')
 
 plt.scatter(dane_m['YEAR'], dane_m['Numeric'], label='Mężczyźni')
 plt.scatter(dane_k['YEAR'], dane_k['Numeric'], label='Kobiety')
