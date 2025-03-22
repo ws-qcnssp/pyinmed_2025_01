9bf1f8d5e7ac97a4b65f24103f1f711f63834ad0
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-03-22 09:54:51 +0100

    2025-03-22 09:54:50

--- exception_handling/exception_handling/concat_1.py
+++ exception_handling/exception_handling/concat_1.py
@@ -8,5 +8,5 @@ string3 = 42
 result1 = concatenate_strings(string1, string2)
 print(result1)
 
-result2 = concatenate_strings(string1, str(string3))
+result2 = concatenate_strings(string1, string3)
 print(result2)
