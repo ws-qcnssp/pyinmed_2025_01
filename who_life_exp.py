32c987536fe9bbf363161ccbb0c30358400b85ce
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-03-22 11:05:40 +0100

    2025-03-22 11:05:40

--- who_life_exp.py
+++ who_life_exp.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('life_expectancy.csv')
 dane = df[['YEAR','REGION','SEX','Numeric']]
-maska = dane['REGION'] == 'GLOBAL'
-dane = dane[maska]
+# maska = dane['REGION'] == 'GLOBAL'
+# dane = dane[maska]
+dane = dane.query('REGION == "GLOBAL"')
+print(dane)
 
