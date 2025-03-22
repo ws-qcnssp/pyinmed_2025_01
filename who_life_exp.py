04156be56f12f225b3cfee866cb86d1672fd6b3b
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-03-22 11:52:37 +0100

    2025-03-22 11:52:37

--- who_life_exp.py
+++ who_life_exp.py
@@ -14,19 +14,19 @@ print(dane)
 dane_m = dane.query('SEX == "MLE"')
 dane_k = dane.query('SEX == "FMLE"')
 
-b_m, a_m = polyfit(dane_m['YEAR'], dane_m['Numeric'], 1)
-b_k, a_k = polyfit(dane_k['YEAR'], dane_k['Numeric'], 1)
+c_m, b_m, a_m = polyfit(dane_m['YEAR'], dane_m['Numeric'], 2)
+c_k, b_k, a_k = polyfit(dane_k['YEAR'], dane_k['Numeric'], 2)
 
-print(f'Dopasowana funkcja dla mężczyzn: f(x) = {a_m} * x + {b_m}')
-print(f'Dopasowana funkcja dla kobiet: f(x) = {a_k} * x + {b_k}')
+print(f'Dopasowana funkcja dla mężczyzn: f(x) = {a_m} * x**2 + {b_m} * x + {c_m}')
+print(f'Dopasowana funkcja dla kobiet: f(x) = {a_k} * x**2 + {b_k} * x + {c_k}')
 
-m_2030 = a_m * 2030 + b_m
+m_2030 = a_m * 2030**2 + b_m*2030 + c_m
 print(f"Oczekiwana długość życia mężczyzn w 2030: {m_2030}")
 
 plt.scatter(dane_m['YEAR'], dane_m['Numeric'], label='Mężczyźni')
 plt.scatter(dane_k['YEAR'], dane_k['Numeric'], label='Kobiety')
-plt.plot(dane_m['YEAR'], a_m * dane_m['YEAR'] + b_m)
-plt.plot(dane_k['YEAR'], a_k * dane_k['YEAR'] + b_k)
+plt.plot(dane_m['YEAR'], a_m * dane_m['YEAR']**2 + b_m * dane_m['YEAR'] + c_m)
+plt.plot(dane_k['YEAR'], a_k * dane_k['YEAR']**2 + b_k * dane_k['YEAR'] + c_k)
 plt.legend()
 plt.title('Długość życia wg WHO - Globalne')
 plt.xlabel('Rok')
