7939715a9d12d7bc507b2de9eded059831f84285
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-05-10 11:28:15 +0200

    2025-05-10 11:28:15

--- check_toxicity.py
+++ check_toxicity.py
@@ -1,9 +1,11 @@
 from playwright.sync_api import sync_playwright
 from web_automation import start_pw, stop_pw
+# import exception_handling.exception_handling.concat_1 as cc
+
 
 def main():
     with sync_playwright() as pw:
         browser, context = start_pw(pw)
         page = context.new_page()
-        
+
         stop_pw(browser, context)
