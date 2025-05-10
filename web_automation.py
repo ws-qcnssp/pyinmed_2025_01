d6ee57be2e11b1ef158e21c731418849fd18e33c
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-05-10 09:49:59 +0200

    2025-05-10 09:49:59

--- web_automation.py
+++ web_automation.py
@@ -1,1 +1,13 @@
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page, Playwright, Browser, BrowserContext
+
+def start_pw(pw: Playwright, headless=False):
+    browser = pw.chromium.launch(
+        headless=headless
+    )
+    context = browser.new_context()
+    return browser, context
+
+def stop_pw(browser: Browser, context: BrowserContext):
+    context.stop()
+    browser.stop()
