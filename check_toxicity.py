891a4f70202b6f5b7762484bd1e868a0ceed0af3
Wojciech Szczypka <szczypka.wojciech@gmail.com>
2025-05-10 11:30:32 +0200

    2025-05-10 11:30:32

--- check_toxicity.py
+++ check_toxicity.py
@@ -2,10 +2,11 @@ from playwright.sync_api import sync_playwright
 from web_automation import start_pw, stop_pw
 # import exception_handling.exception_handling.concat_1 as cc
 
+import easygui as eg
 
 
 def main():
-    
+    chemical = input('Please provide chemical name or CAS number')
     with sync_playwright() as pw:
         browser, context = start_pw(pw)
         page = context.new_page()
